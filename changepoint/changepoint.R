2970aadd2a02a120ca4c6fd37011ca8dddc9ca91
andrea1980s <andrea1980s@gmail.com>
2014-10-12 23:19:08 -0400

    添加生成change point的数据模拟功能； 修改引起gamma function异常的方程错误:去掉值为0的数据和alpha运算

--- changepoint/changepoint.R
+++ changepoint/changepoint.R
@@ -4,124 +4,119 @@ require(MCMCpack)
 ########### Initialization #########
 ####################################
 
-N = 100
-K = 6
-samples = 1
-changepoint.num = 0
-collen = K*(changepoint.num+1)
+K = 6               # category number
+rows = 1            # samples
+changepoint.num = 0 # changepoint number per row 
+#(right now, all the rows has the same changepoint number and position)
+paramsnum = K*(changepoint.num+1) 
+SampleN = 100       # how many points per section have
+rowN = SampleN*(changepoint.num+1)
 
 #####################################
 ########### Data Simulation #########
 #####################################
 
 #simulate the alpha parameter
-alpha.true = unlist(matrix(0,samples,collen))
-for(i in 1:samples){
+alpha.true = unlist(matrix(0,rows,paramsnum))
+for(i in 1:rows){
     x=c()
-    for(j in 1:(changepoint.num+1)){
-        #x = c(x,sort(runif(K,0.5,10)))
-        x = rep.int(1,K)
+    for(j in 1:(changepoint.num+1)){ 
+        y=runif(K,0,0.01*10^j)        # control the change rate
+        y[which(y<0.003*10^j)]=0
+        x = c(x,sort(y*c(1:K)*j))
+        #x= rep.int(1:K,changepoint.num+1)
     }
     alpha.true[i,] = x
+    
 }
 
 #simulate the multinomial data
-data = unlist(matrix(0,samples,N))
-theta.true = unlist(matrix(0,samples,collen))
-for (i in 1:samples){
-    # Dirichlet distribution (from MCMCpack package)
-    theta.true[i,] = rdirichlet(1, alpha.true[i,])
+data = unlist(matrix(0,rows,rowN))
+theta.true = unlist(matrix(0,rows,paramsnum))
+data.n = unlist(matrix(0,rows,paramsnum))
 
+for(i in 1:rows){
+    
+    # Dirichlet distribution (from MCMCpack package)
+    for(j in 1:(changepoint.num+1)){
+        start = K*(j-1)+1 #beginning of jth changepoint section
+        end = K*j           #end of the jth changepoint section
+        # take the avg of the dirichlet simulation
+        # given same alpha,
+        # if sample only once, the result of theta has large diff,
+        # but if sample many times, the mean of theta is similar
+        theta.true[i,start:end] = colMeans(rdirichlet(100, alpha.true[i,start:end]))
+    }
+    
+    
     # Multinomial distribution
-    counts = rmultinom(samples, size = N, prob = theta.true[i,])
+    data.n[i,] = t(rmultinom(1, size = rowN, prob = theta.true[i,]))
+    
+    seeds = rep.int(rep.int(1:K,changepoint.num+1),sapply(data.n[i,],rep))
     
     #sampling to generate the data
-    vector = rep.int(1:K,counts)
-    data[i,] = sample(vector,N,replace=FALSE) 
-}
-
-
-################################################
-########### Known parameters from Data #########
-################################################
-
-# calculate the n,p
-data.p = unlist(matrix(0,samples,collen))
-data.n = unlist(matrix(0,samples,collen))
-true.ctgr = table(factor(rep(1:K,0),levels = 1:K))
-for (i in 1:samples){
-    curr.data.ctgr = table(data[i,])
-    curr.data.Freq = as.data.frame(curr.data.ctgr)$Freq
-    # in case some ctgrs are not sampled
-    if(nrow(curr.data.ctgr) != K){
-        temp = merge(curr.data.ctgr,true.ctgr,by='Var1',all=TRUE)
-        temp$Freq.x[is.na(temp$Freq.x)]=0
-        temp$sum<-temp$Freq.x + temp$Freq.y
-        #sort the level index
-        # -- after merge two ctgrs, the levels index would be confused
-        # -- need to sort the ctgrs levels as 1:K
-        ctgridx = sort(as.numeric(levels(temp$Var1)),index.return=TRUE)$ix
-        curr.data.Freq = temp$sum[ctgridx]
+    for(j in 1:(changepoint.num+1)){
+        start = SampleN*(j-1)+1 # beginning of jth changepoint sample
+        end = SampleN*j
+        data[i,start:end] = sample(seeds[start:end],SampleN,replace=FALSE) 
     }
-    data.n[i,]=curr.data.Freq
-    data.p[i,]=curr.data.Freq/N
-}
+}   
+
+# #################################################################
+# ################# Plot the Data(1 change points) ################
+# #################################################################
+# layout(matrix(c(1,1,2,3,4,5,6,7), 4, 2, byrow = TRUE))
+# plot(1:rowN,data[i,],main="data",xlab = "", ylab = "")
+# hist(data[i,1:SampleN],main="counts",ylim=c(0,max(data.n[i,])),xlim=c(1,K),xlab = "", ylab = "")
+# hist(data[i,(SampleN+1):rowN],main="counts",ylim=c(0,max(data.n[i,])),xlim=c(1,K),xlab = "", ylab = "")
+# plot(1:K,alpha.true[i,1:K],main="alpha",type="b",ylim=c(0,max(alpha.true[i,])),xlab = "", ylab = "")
+# plot(1:K,alpha.true[i,(K+1):paramsnum],main="alpha",type="b",ylim=c(0,max(alpha.true[i,])),xlab = "", ylab = "")
+# plot(1:K,theta.true[i,1:K],main="theta",type="b",ylim=c(0,1),xlab = "", ylab = "")
+# plot(1:K,theta.true[i,(K+1):paramsnum],main="theta",type="b",ylim=c(0,1),xlab = "", ylab = "")
+# 
+
 
 ########################################################
 ########### Grediant Descent to estimate alpha #########
 ########################################################
 
 # initialize the estimated alpha by random value first
-alpha.estimate = unlist(matrix(runif(samples*collen,0.1,1),samples,collen))
+alpha.estimate = unlist(matrix(runif(rows*paramsnum,0.1,1),rows,paramsnum))
 #constriant: alpha > 0
 alpha.estimate[i,which(alpha.estimate[i,] < 0)] = 
     -log10(-alpha.estimate[i,which(alpha.estimate[i,] < 0)])
 
-theta.estimate = unlist(matrix(0,samples,collen))
+theta.estimate = unlist(matrix(0,rows,paramsnum))
 
-Loglikelihood.estimate = unlist(matrix(0,samples,1))
-Loglikelihood.true = unlist(matrix(0,samples,1))
+Loglikelihood.estimate = unlist(matrix(0,rows,1))
+Loglikelihood.true = unlist(matrix(0,rows,1))
 
-for(i in 1:samples){
-    Loglikelihood.true[i,] = - log10(gamma(sum(alpha.true[i,]))) + log10(gamma(sum(alpha.true[i,]) + N)) - 
-        sum(log10(gamma(alpha.true[i,] + data.n[i,])) + log10(gamma(alpha.true[i,]))) -
-        log10(prod(sum(data.n))) - sum(log10(prod(data.n)))
+for(i in 1:rows){
+    idx = which(alpha.true[i,]>0)
+    Loglikelihood.true[i,] = - lgamma(sum(alpha.true[i,])) + lgamma(sum(alpha.true[i,]) + rowN) - 
+        sum(lgamma(alpha.true[i,idx] + data.n[i,idx]) + lgamma(alpha.true[i,idx])) -
+        log10(prod(sum(data.n))) - sum(log10(prod(data.n[idx])))
     
     prelogL = Inf
     # initialize the gradient value by random value first, it will be updated later
     gradient = runif(K) 
     
-    numIterations = 1000000000
+    #current log likelihood
+    idx = which(data.n[i,]>0)
+    currlogL = - lgamma(sum(alpha.estimate[i,])) + lgamma(sum(alpha.estimate[i,]) + rowN) - 
+        sum(lgamma(alpha.estimate[i,idx] + data.n[i,idx]) + lgamma(alpha.estimate[i,idx])) -
+        log10(prod(sum(data.n))) - sum(log10(prod(data.n[idx])))
     
-    for(j in 1:numIterations){
+    while(prelogL - currlogL > 0.00000000001){
 
-        #current log likelihood
-        #currlogL = N * (log10(gamma(sum(alpha.estimate[i,]))) 
-         #               - sum(log10(gamma(alpha.estimate[i,]))) 
-        #                + sum((alpha.estimate[i,] - 1) * log.theta_t.hat))
-        currlogL = - log10(gamma(sum(alpha.estimate[i,]))) + log10(gamma(sum(alpha.estimate[i,]) + N)) - 
-                    sum(log10(gamma(alpha.estimate[i,] + data.n[i,])) + log10(gamma(alpha.estimate[i,])))
         
-        if(j %% 10000 == 0){
-            print(c(currlogL,prelogL,Loglikelihood.true[i,],prelogL - currlogL))
-        }
-        
-        #check whether it's near the optimum
-        if(prelogL - currlogL< 0.00000000001 && j > 1)
-            break
-        
-        prelogL = currlogL 
+        print(prelogL - currlogL)
         
         #update the gradient
-        #gradient = N * (digamma(sum(alpha.estimate[i,])) 
-         #               - digamma(alpha.estimate[i,]) + log.theta_t.hat)
-        gradient = - digamma(sum(alpha.estimate[i,])) + digamma(sum(alpha.estimate[i,]) + N) - 
-                    digamma(alpha.estimate[i,] + data.n[i,]) + digamma(alpha.estimate[i,])
+        gradient[idx] = - digamma(sum(alpha.estimate[i,])) + digamma(sum(alpha.estimate[i,]) + rowN) - 
+                    digamma(alpha.estimate[i,idx] + data.n[i,idx]) + digamma(alpha.estimate[i,idx])
         
         #update alpha.estimate
-        #alpha.estimate[i,] = alpha.estimate[i,]*((digamma(alpha.estimate[i,] + data.n[i,]) - 
-        #                    digamma(alpha.estimate[i,]))/(digamma(sum(alpha.estimate[i,]) + 
-        #                                            N) - digamma(sum(alpha.estimate[i,]))))
         alpha.estimate[i,] = alpha.estimate[i,] + 0.000000005 * gradient
         #constriant: alpha > 0
         while(length(which(alpha.estimate[i,] < 0)) > 0){
@@ -129,12 +124,20 @@ for(i in 1:samples){
                                -log10(-alpha.estimate[i,which(alpha.estimate[i,] < 0)])
         }
         
+        alpha.estimate[i,-idx] = 0
+        
+        prelogL = currlogL 
+        #current log likelihood
+        idx = which(data.n[i,]>0)
+        currlogL = - lgamma(sum(alpha.estimate[i,])) + lgamma(sum(alpha.estimate[i,]) + rowN) - 
+            sum(lgamma(alpha.estimate[i,idx] + data.n[i,idx]) + lgamma(alpha.estimate[i,idx])) -
+            log10(prod(sum(data.n))) - sum(log10(prod(data.n[idx])))
     }
     Loglikelihood.estimate[i,] = currlogL
     #smooth
-    alpha.estimate[i,] = alpha.estimate[i,] + 0.01 * min(alpha.estimate[i,which(alpha.estimate[i,]>0)])
+    #alpha.estimate[i,] = alpha.estimate[i,] + 0.01 * min(alpha.estimate[i,which(alpha.estimate[i,]>0)])
     
-    theta.estimate[i,] = rdirichlet(1,alpha.estimate[i,])
+    theta.estimate[i,] = colMeans(rdirichlet(100, alpha.estimate[i,]))
 }
 
 
--- changepoint/changepoint2.R
+++ changepoint/changepoint2.R
@@ -4,110 +4,116 @@ require(MCMCpack)
 ########### Initialization #########
 ####################################
 
-N = 100
-K = 6
-samples = 1
-changepoint.num = 0
-collen = K*(changepoint.num+1)
+K = 6               # category number
+rows = 1            # samples
+changepoint.num = 0 # changepoint number per row 
+#(right now, all the rows has the same changepoint number and position)
+paramsnum = K*(changepoint.num+1) 
+SampleN = 100       # how many points per section have
+rowN = SampleN*(changepoint.num+1)
 
 #####################################
 ########### Data Simulation #########
 #####################################
 
 #simulate the alpha parameter
-alpha.true = unlist(matrix(0,samples,collen))
-for(i in 1:samples){
+alpha.true = unlist(matrix(0,rows,paramsnum))
+for(i in 1:rows){
     x=c()
-    for(j in 1:(changepoint.num+1)){
-        #x = c(x,sort(runif(K,0.5,10)))
-        x = rep.int(1,K)
+    for(j in 1:(changepoint.num+1)){ 
+        y=runif(K,0,0.01*10^j)        # control the change rate
+        y[which(y<0.003*10^j)]=0
+        x = c(x,sort(y*c(1:K)*j))
+        #x= rep.int(1:K,changepoint.num+1)
     }
     alpha.true[i,] = x
+    
 }
 
 #simulate the multinomial data
-data = unlist(matrix(0,samples,N))
-theta.true = unlist(matrix(0,samples,collen))
-for (i in 1:samples){
+data = unlist(matrix(0,rows,rowN))
+theta.true = unlist(matrix(0,rows,paramsnum))
+data.n = unlist(matrix(0,rows,paramsnum))
+
+for(i in 1:rows){
+    
     # Dirichlet distribution (from MCMCpack package)
-    theta.true[i,] = rdirichlet(1, alpha.true[i,])
+    for(j in 1:(changepoint.num+1)){
+        start = K*(j-1)+1 #beginning of jth changepoint section
+        end = K*j           #end of the jth changepoint section
+        # take the avg of the dirichlet simulation
+        # given same alpha,
+        # if sample only once, the result of theta has large diff,
+        # but if sample many times, the mean of theta is similar
+        theta.true[i,start:end] = colMeans(rdirichlet(100, alpha.true[i,start:end]))
+    }
+    
     
     # Multinomial distribution
-    counts = rmultinom(samples, size = N, prob = theta.true[i,])
+    data.n[i,] = t(rmultinom(1, size = rowN, prob = theta.true[i,]))
+    
+    seeds = rep.int(rep.int(1:K,changepoint.num+1),sapply(data.n[i,],rep))
     
     #sampling to generate the data
-    vector = rep.int(1:K,counts)
-    data[i,] = sample(vector,N,replace=FALSE) 
-}
-
-
-################################################
-########### Known parameters from Data #########
-################################################
-
-# calculate the n,p
-data.p = unlist(matrix(0,samples,collen))
-data.n = unlist(matrix(0,samples,collen))
-true.ctgr = table(factor(rep(1:K,0),levels = 1:K))
-for (i in 1:samples){
-    curr.data.ctgr = table(data[i,])
-    curr.data.Freq = as.data.frame(curr.data.ctgr)$Freq
-    # in case some ctgrs are not sampled
-    if(nrow(curr.data.ctgr) != K){
-        temp = merge(curr.data.ctgr,true.ctgr,by='Var1',all=TRUE)
-        temp$Freq.x[is.na(temp$Freq.x)]=0
-        temp$sum<-temp$Freq.x + temp$Freq.y
-        #sort the level index
-        # -- after merge two ctgrs, the levels index would be confused
-        # -- need to sort the ctgrs levels as 1:K
-        ctgridx = sort(as.numeric(levels(temp$Var1)),index.return=TRUE)$ix
-        curr.data.Freq = temp$sum[ctgridx]
+    for(j in 1:(changepoint.num+1)){
+        start = SampleN*(j-1)+1 # beginning of jth changepoint sample
+        end = SampleN*j
+        data[i,start:end] = sample(seeds[start:end],SampleN,replace=FALSE) 
     }
-    data.n[i,]=curr.data.Freq
-    data.p[i,]=curr.data.Freq/N
-}
+}   
+
+# #################################################################
+# ################# Plot the Data(1 change points) ################
+# #################################################################
+# layout(matrix(c(1,1,2,3,4,5,6,7), 4, 2, byrow = TRUE))
+# plot(1:rowN,data[i,],main="data",xlab = "", ylab = "")
+# hist(data[i,1:SampleN],main="counts",ylim=c(0,max(data.n[i,])),xlim=c(1,K),xlab = "", ylab = "")
+# hist(data[i,(SampleN+1):rowN],main="counts",ylim=c(0,max(data.n[i,])),xlim=c(1,K),xlab = "", ylab = "")
+# plot(1:K,alpha.true[i,1:K],main="alpha",type="b",ylim=c(0,max(alpha.true[i,])),xlab = "", ylab = "")
+# plot(1:K,alpha.true[i,(K+1):paramsnum],main="alpha",type="b",ylim=c(0,max(alpha.true[i,])),xlab = "", ylab = "")
+# plot(1:K,theta.true[i,1:K],main="theta",type="b",ylim=c(0,1),xlab = "", ylab = "")
+# plot(1:K,theta.true[i,(K+1):paramsnum],main="theta",type="b",ylim=c(0,1),xlab = "", ylab = "")
+
 
 ########################################################
 ########### optim method to estimate alpha #########
 ########################################################
 # define the function to be optimized
 loglikelihood <- function(par,data.n){
-    log10(gamma(sum(par))) - log10(gamma(sum(par + data.n))) + 
-        sum(log10(gamma(par + data.n)) + log10(gamma(par))) +
-        log10(prod(sum(data.n))) + sum(log10(prod(data.n)))
+    idx = which(data.n>0)
+    lgamma(sum(par)) - lgamma(sum(par + data.n)) + 
+        sum(lgamma(par[idx] + data.n[idx]) + lgamma(par[idx])) +
+        log10(prod(sum(data.n))) + sum(log10(prod(data.n[idx])))
 }
 
 gradient <- function(par,data.n){ ##Gradient of loglikelihood
-    digamma(sum(par)) - digamma(sum(par + data.n)) + 
-        digamma(par + data.n) - digamma(par)
+    idx = which(data.n>0)
+    gdt = unlist(matrix(0,1,length(par)))
+    gdt[idx] = digamma(sum(par)) - digamma(sum(par + data.n)) + 
+        digamma(par[idx] + data.n[idx]) - digamma(par[idx])
+    gdt
 }
 
 # initialize the estimated alpha by random value first
-alpha.estimate = unlist(matrix(runif(samples*collen,0.1,1),samples,collen))
-#constriant: alpha > 0
-alpha.estimate[i,which(alpha.estimate[i,] < 0)] = 
-    -log10(-alpha.estimate[i,which(alpha.estimate[i,] < 0)])
-theta.estimate = unlist(matrix(0,samples,collen))
+alpha.estimate = unlist(matrix(runif(rows*paramsnum,0.1,1),rows,paramsnum))
+theta.estimate = unlist(matrix(0,rows,paramsnum))
 
-Loglikelihood.estimate = unlist(matrix(0,samples,1))
-Loglikelihood.true = unlist(matrix(0,samples,1))
+Loglikelihood.estimate = unlist(matrix(0,rows,1))
+Loglikelihood.true = unlist(matrix(0,rows,1))
 
-for(i in 1:samples){
-    Loglikelihood.true[i] = log10(gamma(sum(alpha.true[i,]))) - 
-                    log10(gamma(sum(alpha.true[i,]) + N)) + 
-                    sum(log10(gamma(alpha.true[i,] + data.n[i,])) + 
-                    log10(gamma(alpha.true[i,])))
+for(i in 1:rows){
+    Loglikelihood.true[i] =  loglikelihood(alpha.true[i,],data.n[i,])
+    
+    alpha.estimate[i,which(data.n[i,] == 0)] = 0
+    
     res = optim(alpha.estimate[i,],loglikelihood,gradient,data.n = data.n[i,], 
-            method="L-BFGS-B", lower=rep(0.000000001,length(alpha.estimate[i,])),
-            upper=rep(Inf,length(alpha.estimate[i,])),
+            method="L-BFGS-B", lower=rep(0,paramsnum), upper=rep(Inf,paramsnum),
             control=list(fnscale=-1,ndeps=1e-10,maxit=1e+9))
     alpha.estimate[i,] = res$par
     #smooth
-    alpha.estimate[i,] = alpha.estimate[i,] + 0.01 * min(alpha.estimate[i,which(alpha.estimate[i,]>0)])
+    #alpha.estimate[i,] = alpha.estimate[i,] + 0.01 * min(alpha.estimate[i,which(alpha.estimate[i,]>0)])
     
     Loglikelihood.estimate[i,] = res$value
-    theta.estimate[i,] = rdirichlet(1,alpha.estimate[i,])
+    theta.estimate[i,] = colMeans(rdirichlet(100, alpha.estimate[i,]))
+    
 }
-
-
-
