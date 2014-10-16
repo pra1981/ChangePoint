1c9bf63ada70448e2374f83305bd8c9f2550b501
andrea1980s <andrea1980s@gmail.com>
2014-10-15 23:33:55 -0400

    新增代码提交 - 计算argmax公式10 - 未完成

--- changepoint/changepoint2.R
+++ changepoint/changepoint2.R
@@ -4,7 +4,7 @@ require(MCMCpack)
 ########### Initialization #########
 ####################################
 
-K = 6               # category number
+K = 5               # category number
 rows = 1            # samples
 changepoint.num = 0 # changepoint number per row 
 #(right now, all the rows has the same changepoint number and position)
@@ -17,44 +17,37 @@ rowN = SampleN*(changepoint.num+1)
 #####################################
 
 #simulate the alpha parameter
-alpha.true = unlist(matrix(0,rows,paramsnum))
-for(i in 1:rows){
-    x=c()
-    for(j in 1:(changepoint.num+1)){ 
-        y=runif(K,0,0.01*10^j)        # control the change rate
-        y[which(y<0.003*10^j)]=0
-        x = c(x,sort(y*c(1:K)*j))
-        #x= rep.int(1:K,changepoint.num+1)
-    }
-    alpha.true[i,] = x
-    
+x=c()
+for(j in 1:(changepoint.num+1)){ 
+    # control the change rate
+    x = c(x,unlist(lapply(c(1:K),dbinom,size=5,prob=runif(1,0.9,1))))
+    #x= rep.int(1:K,changepoint.num+1)
 }
+alpha.true = matrix(x,1, paramsnum)
 
 #simulate the multinomial data
 data = unlist(matrix(0,rows,rowN))
 theta.true = unlist(matrix(0,rows,paramsnum))
 data.n = unlist(matrix(0,rows,paramsnum))
 
+# Dirichlet distribution
+for(j in 1:(changepoint.num+1)){
+    start = K*(j-1)+1 #beginning of jth changepoint section
+    end = K*j           #end of the jth changepoint section
+    # take the avg of the dirichlet simulation
+    # given same alpha,
+    # if sample only once, the result of theta has large diff,
+    # but if sample many times, the mean of theta is similar
+    theta.true[,start:end] = rdirichlet(rows, alpha.true[start:end])
+}
+
+
 for(i in 1:rows){
-    
-    # Dirichlet distribution (from MCMCpack package)
-    for(j in 1:(changepoint.num+1)){
-        start = K*(j-1)+1 #beginning of jth changepoint section
-        end = K*j           #end of the jth changepoint section
-        # take the avg of the dirichlet simulation
-        # given same alpha,
-        # if sample only once, the result of theta has large diff,
-        # but if sample many times, the mean of theta is similar
-        theta.true[i,start:end] = colMeans(rdirichlet(100, alpha.true[i,start:end]))
-    }
-    
-    
     # Multinomial distribution
     data.n[i,] = t(rmultinom(1, size = rowN, prob = theta.true[i,]))
     
+    #sampling to generate the data  -- for illustrate purpose
     seeds = rep.int(rep.int(1:K,changepoint.num+1),sapply(data.n[i,],rep))
-    
-    #sampling to generate the data
     for(j in 1:(changepoint.num+1)){
         start = SampleN*(j-1)+1 # beginning of jth changepoint sample
         end = SampleN*j
@@ -66,7 +59,7 @@ for(i in 1:rows){
 # ################# Plot the Data(1 change points) ################
 # #################################################################
 # layout(matrix(c(1,1,2,3,4,5,6,7), 4, 2, byrow = TRUE))
-# plot(1:rowN,data[i,],main="data",xlab = "", ylab = "")
+# plot(1:rowN,data[i,],main="data",xlab = "", ylab = "",ylim=c(0,K))
 # hist(data[i,1:SampleN],main="counts",ylim=c(0,max(data.n[i,])),xlim=c(1,K),xlab = "", ylab = "")
 # hist(data[i,(SampleN+1):rowN],main="counts",ylim=c(0,max(data.n[i,])),xlim=c(1,K),xlab = "", ylab = "")
 # plot(1:K,alpha.true[i,1:K],main="alpha",type="b",ylim=c(0,max(alpha.true[i,])),xlab = "", ylab = "")
@@ -102,7 +95,7 @@ Loglikelihood.estimate = unlist(matrix(0,rows,1))
 Loglikelihood.true = unlist(matrix(0,rows,1))
 
 for(i in 1:rows){
-    Loglikelihood.true[i] =  loglikelihood(alpha.true[i,],data.n[i,])
+    Loglikelihood.true[i] =  loglikelihood(alpha.true,data.n[i,])
     
     alpha.estimate[i,which(data.n[i,] == 0)] = 0
     
@@ -117,3 +110,17 @@ for(i in 1:rows){
     theta.estimate[i,] = colMeans(rdirichlet(100, alpha.estimate[i,]))
     
 }
+
+###############################################################
+################## One Change Point Model  ####################
+###############################################################
+alpha.alpha = vector("list",rowN)
+alpha.beta = vector("list",rowN)
+
+for(lambda in 1:rowN-1){
+    alpha = unlist(matrix(runif(paramsnum,0.1,1),1,paramsnum))
+    beta = unlist(matrix(runif(paramsnum,0.1,1),1,paramsnum))
+    
+    
+}
+
